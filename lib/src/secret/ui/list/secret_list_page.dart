f74f17c31077b784e67f5b2980b5f03bf0ee24aa
timoweber <timo_weber@outlook.com>
2020-12-28 18:00:27 +0100

    feature/6: add search feature for secrets list, closes #6

--- lib/src/secret/ui/list/secret_list_page.dart
+++ lib/src/secret/ui/list/secret_list_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -6,8 +8,10 @@ import 'package:lokr_ui/src/messaging_service.dart';
 import 'package:lokr_ui/src/secret/bloc/secrets_bloc.dart';
 import 'package:lokr_ui/src/secret/bloc/secrets_event.dart';
 import 'package:lokr_ui/src/secret/bloc/secrets_state.dart';
+import 'package:lokr_ui/src/secret/domain/secret.dart';
 import 'package:lokr_ui/src/secret/ui/detail/secret_detail_page.dart';
 import 'package:lokr_ui/src/secret/ui/list/secret_list_item.dart';
+import 'package:lokr_ui/src/secret/ui/lokrui_text_form_field.dart';
 import 'package:lokr_ui/src/secret/ui/secret_page_header.dart';
 
 class SecretListPage extends StatelessWidget {
@@ -78,6 +82,37 @@ class _RefreshableListView extends StatefulWidget {
 }
 
 class _RefreshableListViewState extends State<_RefreshableListView> {
+  String _filterPattern = '';
+  final TextEditingController _searchController = TextEditingController();
+
+  void _onSearchUpdate() {
+    print('on search update');
+    setState(() {
+      _filterPattern = _searchController.text;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this._searchController.addListener(_onSearchUpdate);
+  }
+
+  @override
+  void dispose() {
+    this._searchController.dispose();
+    super.dispose();
+  }
+
+  bool _matchesFilterPattern(Secret secret) {
+    return (secret.title
+        .toLowerCase()
+        .contains(this._filterPattern.toLowerCase()) ||
+        (secret.description != null &&
+            secret.description.toLowerCase().contains(
+                this._filterPattern.toLowerCase())));
+  }
+
   @override
   Widget build(BuildContext context) {
     Future<void> _refreshState() async {
@@ -86,55 +121,78 @@ class _RefreshableListViewState extends State<_RefreshableListView> {
     }
 
     _refreshState();
+    return Column(
+      children: [
+        LOKRUITextFormField(
+            label: tr('pages.list.body.search.label'),
+            suffix: Icon(Icons.search),
+            controller: this._searchController),
+        Expanded(
+          child: RefreshIndicator(
+              child: BlocBuilder<SecretsBloc, SecretsState>(
+                builder: (context, state) {
+                  if (state is SecretsInitial) {
+                    return Padding(
+                        padding: EdgeInsets.only(top: 8),
+                        child: Column(
+                          children: [
+                            Text(tr('pages.list.body.secretList.loading'),
+                                style: Theme.of(context).textTheme.bodyText2),
+                            Padding(
+                              padding: EdgeInsets.only(top: 16),
+                              child: LinearProgressIndicator(),
+                            )
+                          ],
+                        ));
+                  } else if (state is SecretsFetchAllSuccess) {
+                    final List<SecretListItem> filteredListItems = state.secrets
+                        .where(_matchesFilterPattern)
+                        .map((e) => SecretListItem(e))
+                        .toList();
 
-    return RefreshIndicator(
-        child: BlocBuilder<SecretsBloc, SecretsState>(
-          builder: (context, state) {
-            if (state is SecretsInitial) {
-              return Padding(
-                  padding: EdgeInsets.only(top: 8),
-                  child: Column(
-                    children: [
-                      Text(tr('pages.list.body.secretList.loading'),
-                          style: Theme.of(context).textTheme.bodyText2),
-                      Padding(
-                        padding: EdgeInsets.only(top: 16),
-                        child: LinearProgressIndicator(),
-                      )
-                    ],
-                  ));
-            } else if (state is SecretsFetchAllSuccess) {
-              return state.secrets.isEmpty
-                  ? Padding(
-                      padding: EdgeInsets.only(top: 8),
-                      child: Text(tr('pages.list.body.secretList.empty'),
-                          style: Theme.of(context).textTheme.bodyText2))
-                  : ListView(
-                      shrinkWrap: true,
-                      children:
-                          state.secrets.map((e) => SecretListItem(e)).toList(),
-                    );
-            } else {
-              return Padding(
-                  padding: EdgeInsets.only(top: 8),
-                  child: Column(
-                    children: [
-                      Text(tr('pages.list.body.secretList.error'),
-                          style: Theme.of(context).textTheme.bodyText2),
-                      Padding(
+                    if (state.secrets.isEmpty)
+                      return Padding(
                           padding: EdgeInsets.only(top: 8),
-                          child: ElevatedButton(
-                            onPressed: () => {
-                              BlocProvider.of<SecretsBloc>(context)
-                                  .add(SecretsFetchAll())
-                            },
-                            child: Text(tr('pages.list.body.secretList.retry')),
-                          )),
-                    ],
-                  ));
-            }
-          },
+                          child: Text(tr('pages.list.body.secretList.empty'),
+                              style: Theme.of(context).textTheme.bodyText2));
+                    else if (filteredListItems.isEmpty) {
+                      return Padding(
+                          padding: EdgeInsets.only(top: 8),
+                          child: Text(tr('pages.list.body.search.noResult'),
+                              style: Theme.of(context).textTheme.bodyText2));
+                    } else {
+                      return ListView(
+                        shrinkWrap: true,
+                        children: filteredListItems,
+                      );
+                    }
+                  } else {
+                    return Padding(
+                        padding: EdgeInsets.only(top: 8),
+                        child: Column(
+                          children: [
+                            Text(tr('pages.list.body.secretList.error'),
+                                style: Theme.of(context).textTheme.bodyText2),
+                            Padding(
+                                padding: EdgeInsets.only(top: 8),
+                                child: ElevatedButton(
+                                  onPressed: () => {
+                                    BlocProvider.of<SecretsBloc>(context)
+                                        .add(SecretsFetchAll())
+                                  },
+                                  child: Text(
+                                      tr('pages.list.body.secretList.retry')),
+                                )),
+                          ],
+                        ));
+                  }
+                },
+              ),
+              onRefresh: _refreshState),
         ),
-        onRefresh: _refreshState);
+      ],
+    );
   }
 }
+
+
